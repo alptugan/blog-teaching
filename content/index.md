850281bd5481eeba0086d09add265e2be5a1d645
alp tuğan <alptugan@gmail.com>
2024-04-11 17:46:52 +0300

    Quartz sync: Apr 11, 2024, 5:46 PM

--- content/index.md
+++ content/index.md
@@ -2,9 +2,8 @@
 title: Home
 ---
 The following repository includes Alp Tuğan's creative coding and sound design related content.
-🔗 Navigate to the [teaching blog](https://alptugan.github.io/blog-teaching/)  
 
-## Credits
+The following content is built using following open-source components;
 * [Quartz 4.0](https://quartz.jzhao.xyz/) static web-site builder tool.
 * [Obsidian](https://obsidian.md/) Note Taking App.
 * Obsidian [Templater Plugin](https://silentvoid13.github.io/Templater/internal-functions/internal-modules/date-module.html)
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,6 +22,118 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
+    // Component.Divider(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.Divider(),
+    Component.DesktopOnly(Component.RecentNotes({
+      title: "Most recent",
+      limit: 5
+    })),
+    Component.DesktopOnly(Component.Explorer({
+      title: "Explore",
+      useSavedState: true,
+      sortFn: (a, b) => {
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          return a.displayName.localeCompare(b.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+        if (a.file && !b.file) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+    })),
+  ],
+  middle: [],
+  right: [
+    Component.TagList(),
+    Component.Graph({
+      localGraph: {
+        linkDistance: 50,
+      },
+      globalGraph: {
+        linkDistance: 50,
+      },
+    }),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
+    Component.MobileOnly(Component.RecentNotes({
+      title: "Most recent",
+      limit: 5
+    })),
+    Component.MobileOnly(Component.Explorer({
+      title: "Explore",
+      useSavedState: true,
+      sortFn: (a, b) => {
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          return a.displayName.localeCompare(b.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+        if (a.file && !b.file) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+    })),
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.ArticleTitle()],
+  left: [
+    Component.PageTitle(),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.Divider(),
+    Component.DesktopOnly(Component.RecentNotes({
+      title: "Most recent",
+      limit: 5
+    })),
+    Component.DesktopOnly(Component.Explorer({
+      title: "Explore",
+      useSavedState: true,
+      sortFn: (a, b) => {
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          return a.displayName.localeCompare(b.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+        if (a.file && !b.file) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+    })),
+  ],
+  middle: [],
+  right: [],
+}
+
+// components for pages that display a single page (e.g. a single note)
+/*export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
     Component.TagList(),
   ],
   middle: [
@@ -53,4 +165,4 @@ export const defaultListPageLayout: PageLayout = {
   ],
   middle: [],
   right: [],
-}
+}*/
