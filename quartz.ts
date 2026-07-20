86ec2910bd6a037f9922e218863967a3881e0ede
alp tuğan <alptugan@gmail.com>
2026-07-20 11:29:08 +0300

    Quartz sync: Jul 20, 2026, 11:29 AM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,14 @@
+import * as ExternalPlugin from "./.quartz/plugins"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
 
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
+
+ExternalPlugin.Explorer({
+  filterFn: (node) => {
+    // Hides the assets folder from the sidebar
+    return node.displayName.toLowerCase() !== "assets"
+  }
+})
